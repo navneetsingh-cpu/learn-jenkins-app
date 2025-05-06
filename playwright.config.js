f184b0837ee3fe84372e755c8bf92d8a0a81a157
Navneet Singh <navneetsingh17736@gmail.com>
2025-05-05 22:03:46 -0400

    Fixed junit

--- playwright.config.js
+++ playwright.config.js
@@ -23,8 +23,8 @@ module.exports = defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html'],
-    ['junit']
-  ],
+    ['junit', { outputFile: 'test-results-e2e/junit.xml' }]
+  ]
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
